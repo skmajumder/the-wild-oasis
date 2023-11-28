c4e279b7af11985e9f383a09cecc2b2560448f36
skmajumder <skmajumder.cse@gmail.com>
2023-11-28 20:17:01 +0600

    styled components div

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
 import styled from "styled-components";
 
 const H1 = styled.h1`
-  font-size: 70px;
-  font-weight: 700;
+  font-size: 40px;
   text-transform: capitalize;
+  font-weight: 600;
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+`;
+
+const StyledApp = styled.div`
+  background-color: aliceblue;
+  padding: 20px;
 `;
 
 const App = () => {
   return (
-    <div>
+    <StyledApp>
       <H1>The wild oasis</H1>
-    </div>
+    </StyledApp>
   );
 };
 
