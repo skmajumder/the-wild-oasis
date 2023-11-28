6fb5ca570c378272ab3b62ac22a31d5127192ab7
skmajumder <skmajumder.cse@gmail.com>
2023-11-28 20:01:27 +0600

    styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import styled from "styled-components";
+
+const H1 = styled.h1`
+  font-size: 70px;
+  font-weight: 700;
+  text-transform: capitalize;
+`;
+
 const App = () => {
   return (
     <div>
-      <h1>The Wild Oasis</h1>
+      <H1>The wild oasis</H1>
     </div>
   );
 };
