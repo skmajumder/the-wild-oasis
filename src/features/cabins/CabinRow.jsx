e2d66a015c8a06307f8ef7f571c19287a091ed72
skmajumder <skmajumder.cse@gmail.com>
2023-12-04 19:30:22 +0600

    fetching, stale & fresh data

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -1,11 +1,12 @@
 import styled from "styled-components";
+import { formatCurrency } from "../../utils/helpers";
 
 const TableRow = styled.div`
   display: grid;
   grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
   column-gap: 2.4rem;
   align-items: center;
-  padding: 1.4rem 2.4rem;
+  padding: 1.4rem 3rem;
   &:not(:last-child) {
     border-bottom: 1px solid var(--color-grey-100);
   }
@@ -14,6 +15,7 @@ const TableRow = styled.div`
 const Image = styled.img`
   display: block;
   width: 6.4rem;
+  border-radius: 2px;
   aspect-ratio: 3 / 2;
   object-fit: cover;
   object-position: center;
@@ -46,8 +48,9 @@ const CabinRow = ({ cabin }) => {
       <Image src={image} alt={name} />
       <Cabin>{name}</Cabin>
       <p>Fits up to {maxCapacity} guests</p>
-      <Price>{regularPrice}</Price>
-      <Discount>{discount ? discount : "--"}</Discount>
+      <Price>{formatCurrency(regularPrice)}</Price>
+      <Discount>{discount ? formatCurrency(discount) : "--"}</Discount>
+      <button>Delete</button>
     </TableRow>
   );
 };
